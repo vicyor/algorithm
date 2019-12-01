b6a70af45b3ddb6df87babc5e519525cc4e390f7
vicyor <2457569580@qq.com>
2019-12-01 19:34:36 +0800

    第三题 二个递归实现栈逆置

---
+++ src/main/java/com/vicyor/algorithm/Question_3.java
@@ -0,0 +1,45 @@
+package com.vicyor.algorithm;
+
+import java.util.Stack;
+
+/*
+    如何用递归函数和栈操作来完成逆序一个栈
+ */
+public class Question_3 {
+    public static void main(String[] args) {
+    }
+}
+
+//我不会
+//官方解
+class Question_3_Slove {
+    Stack<Integer> stack = new Stack();
+
+    void print() {
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    void push(Integer i) {
+        stack.push(i);
+    }
+
+    //移除栈的最后一个元素并返回
+    public Integer recursiveTask() {
+        Integer item = stack.pop();
+        if (stack.isEmpty()) {
+            return item;
+        } else {
+            Integer last = recursiveTask();
+            stack.push(item);
+            return last;
+        }
+    }
+
+    public void reverse() {
+        Integer last = recursiveTask();
+        reverse();
+        stack.push(last);
+    }
+}
