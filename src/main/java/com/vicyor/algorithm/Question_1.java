79ad8e115b39ed2c278dfbbc97339342f8ffa699
vicyor <2457569580@qq.com>
2019-12-01 17:53:25 +0800

    第一题

--- src/main/java/com/vicyor/algorithm/Question_1.java
+++ src/main/java/com/vicyor/algorithm/Question_1.java
@@ -1,9 +1,47 @@
 package com.vicyor.algorithm;
 
-/**
- * 作者:姚克威
- * 时间:2019/12/1 17:31
- **/
+import java.util.Stack;
 
+/**
+ * 题目: 实现一个特殊的栈,在栈的基本功能的基础上,实现返回最小的元素
+ * getMin的时间复杂度为O(1)
+ */
 public class Question_1 {
+
+}
+
+/*
+    官方解 1：
+      用2个栈,
+      getMin 原理
+      a ------> b ------>c
+      即使b到c的元素弹出了,但是a-b的最小值依然保存在栈中
+ */
+class Question_1_S1 {
+    //数据栈
+    Stack<Integer> data = new Stack();
+    //最小栈
+    Stack<Integer> min = new Stack();
+
+    void push(Integer num) {
+        data.push(num);
+        if (min.empty() || min.peek() >= num) {
+            min.push(num);
+        }
+    }
+
+    Integer pop() {
+        Integer pop = null;
+        if (!data.empty()) {
+            pop = data.pop();
+            if (min.peek().equals(pop)) {
+                min.pop();
+            }
+        }
+        return pop;
+    }
+
+    Integer getMin() {
+        return min.empty() ? null : min.peek();
+    }
 }
